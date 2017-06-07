a9d58a165b7f1c2bf8bbfa78c3fbff3778af1691
dstepbw <dstepbw@gmail.com>
2017-06-07 20:59:12 -0300

    adding redux-promise-middleware and axios

--- app/Store.js
+++ app/Store.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import * as reducers from './reducers'
+import promise from 'redux-promise-middleware'
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
+const createStoreWithMiddleware = applyMiddleware(thunk, promise())(createStore)
 const combinedReducers = combineReducers(reducers)
 
 export default createStoreWithMiddleware(combinedReducers)

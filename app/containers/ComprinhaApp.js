d51df1364ce5395633c25a24d80d6ac9299dd23d
dstepbw <dstepbw@gmail.com>
2017-06-07 22:39:56 -0300

    removing styles

--- app/containers/ComprinhaApp.js
+++ app/containers/ComprinhaApp.js
@@ -1,22 +1,7 @@
 import React, { Component } from 'react'
-import { AppRegistry,  StyleSheet, View, Alert } from 'react-native'
 import { NavigationActions, addNavigationHelpers } from 'react-navigation/src/react-navigation';
 import { connect } from 'react-redux'
 import AppNavigator from '../Navigator';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  }
-})
 
 const ComprinhaApp = ({ nav, dispatch }) =>  (
   <AppNavigator navigation={ addNavigationHelpers({ dispatch, state: nav }) } />
